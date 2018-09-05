31a9853ebaea70767b140fbceede56065050df19
serg722 <722kurmach@gmail.com>
2018-09-05 10:23:24 +0300

    User Form 5.09

--- src/eentity/User.java
+++ src/eentity/User.java
@@ -5,9 +5,9 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class User implements Comparable<User>{
-    String email;
-    String password;
-    LocalDate birhday;
+   public String email;
+    public String password;
+    public LocalDate birhday;
 
     static private Set<User> listUser = new TreeSet<>();
 
@@ -18,19 +18,28 @@ public class User implements Comparable<User>{
         listUser.add(this);
     }
 
+   public void printUser(){
+        System.out.println(listUser);
+    }
+
+    public static Set<User> getListUser() {
+        return listUser;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "email='" + email + '\'' +
-                ", password='" + password + '\'' +
                 ", birhday=" + birhday +
                 '}';
     }
 
     public static void main(String[] args) {
 
-        User user1 = new User("mail@i.ua", "qwe123", LocalDate.of(2000, 12, 23));
-        User user2 = new User("mail@i.ua", "qwewwe123", LocalDate.of(2000, 12, 23));
+        User user1 = new User
+                ("mail@i.ua", "qwe123", LocalDate.of(2000, 12, 23));
+        User user2 = new User
+                ("mail@i.ua", "qwewwe123", LocalDate.of(2000, 12, 23));
 
         System.out.println(User.listUser);
         //LocalDate qw=new LocalDate();
--- src/form/UserForm.java
+++ src/form/UserForm.java
@@ -1,20 +1,48 @@
 package form;
 
 
+import eentity.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class UserForm {
-   @FXML
+    @FXML
     Button btn1;
-   @FXML
+    @FXML
     TextField tf_email;
+    @FXML
+    PasswordField textPass;
+    @FXML
+    DatePicker dateBirthdey;
+    @FXML
+    ComboBox UserSpisok;
+    ObservableList<User>  modelUser=FXCollections.observableArrayList(User.getListUser());
 
-   public void one(){
-       System.out.println(tf_email.getText());
-   }
-    public void two(){
-        System.out.println("two");
+    @FXML
+    public void initialize(){
+      UserSpisok.setItems(modelUser);
+    }
+
+
+    public void one() {
+        User user = new User(tf_email.getText(), textPass.getText(), dateBirthdey.getValue());
+        user.printUser();
+        modelUser.add(user);
+        tf_email.setText("");
+        textPass.setText("");
+        dateBirthdey.setValue(null);
+
+    }
+
+
+    public void two() {
+
+        User us=((User)UserSpisok.getValue());
+
+        tf_email.setText(us.email);
+        textPass.setText(us.password);
+        dateBirthdey.setValue(us.birhday);
     }
 }
