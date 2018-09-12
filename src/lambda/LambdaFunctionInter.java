31f770e936a1a84b1949741d324d7af17fee7157
serg722 <722kurmach@gmail.com>
2018-09-12 20:41:30 +0300

    FunctionalInterface 12.09

---
+++ src/lambda/LambdaFunctionInter.java
@@ -0,0 +1,29 @@
+package lambda;
+
+public class LambdaFunctionInter {
+    int x = 9;
+    int y = 5;
+    void sum(LambdaOne sumLam){
+        System.out.println(x+y);
+    }
+
+    public static void main(String[] args) {
+
+
+        LambdaFunctionInter lamb1 = new LambdaFunctionInter();
+        lamb1.sum(new LambdaOne(){
+
+            @Override
+            public int sum(int x, int y) {
+                return x+y;
+            }
+        });
+    }
+
+}
+
+@FunctionalInterface
+
+interface LambdaOne {
+    int sum(int x, int y);
+        }
