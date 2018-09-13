6a9d29d5466c22e00d711976f2bf6a75f4c34289
serg722 <722kurmach@gmail.com>
2018-09-13 20:31:55 +0300

    FunctionalInterface 13.09

--- src/lambda/LambdaFunctionInter.java
+++ src/lambda/LambdaFunctionInter.java
@@ -1,26 +1,31 @@
 package lambda;
 
-ВРОДЕ РАБОТАЕТ. НО КАК-ТО КОРЯВЕНЬКО, ПО МОЕМУ.
+//ВРОДЕ РАБОТАЕТ. НО КАК-ТО КОРЯВЕНЬКО, ПО МОЕМУ.
 
 public class LambdaFunctionInter {
     int x = 9;
     int y = 5;
-    void sum(LambdaOne sumLam){
-        System.out.println(x+y);
+
+    void sum(LambdaOne sumLam) {
+        System.out.println(x + y);
     }
 
     public static void main(String[] args) {
 
 
+
+        ClassLamThree ololo = new ClassLamThree();
+        ololo.arrOlolo(new int[]{5, 8, 9, 10});
+
         ClassLamTwo x = new ClassLamTwo();
         System.out.println(x.trimX("    5 4 4   ", 5));
 
         LambdaFunctionInter lamb1 = new LambdaFunctionInter();
-        lamb1.sum(new LambdaOne(){
+        lamb1.sum(new LambdaOne() {
 
             @Override
             public int sum(int x, int y) {
-                return x+y;
+                return x + y;
             }
         });
     }
@@ -28,28 +33,63 @@ public class LambdaFunctionInter {
 }
 
 @FunctionalInterface
-
 interface LambdaOne {
     int sum(int x, int y);
-        }
-
-@FunctionalInterface
-
-interface LambdaTwo {
-    String trimX(String x, int y);
-        }
-
-@FunctionalInterface
-
-interface LambdaThree {
-    int sum(String x, int y);
 }
 
+@FunctionalInterface
+interface LambdaTwo {
+    String trimX(String x, int y);
+}
 
-        class ClassLamTwo implements LambdaTwo{
+@FunctionalInterface
+interface LambdaThree {
+    void arrOlolo(int[] ololo);
+}
 
-            @Override
-            public String trimX(String x, int y) {
-                return x.trim();
+@FunctionalInterface
+interface LambdaMoreFive {
+
+    int[] moreFive(int[] ololo);
+
+    default int[] isMore(int[] ololo) {
+        int[] ololoMore = new int[ololo.length];
+        for (int i = 0 ; i > ololo.length; i++) {
+            if(ololo[i] > 5){
+                ololoMore[i] = ololo[i];
             }
         }
+        return ololoMore;
+    }
+}
+
+class MoreFive implements LambdaMoreFive{
+
+    @Override
+    public int[] moreFive(int[] ololo) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] isMore(int[] ololo) {
+        return new int[0];
+    }
+}
+
+class ClassLamTwo implements LambdaTwo {
+
+    @Override
+    public String trimX(String x, int y) {
+        return x.trim();
+    }
+}
+
+class ClassLamThree implements LambdaThree {
+
+    @Override
+    public void arrOlolo(int[] ololo) {
+        for (int temp : ololo) {
+            System.out.println(temp);
+        }
+    }
+}
