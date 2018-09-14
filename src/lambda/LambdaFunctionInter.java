e68037d0c45a9f8aa7c2f0b5b31c1dcfb1bd98d0
serg722 <722kurmach@gmail.com>
2018-09-14 17:08:50 +0300

    FunctionalInterface 13.09

--- src/lambda/LambdaFunctionInter.java
+++ src/lambda/LambdaFunctionInter.java
@@ -2,6 +2,10 @@ package lambda;
 
 //ВРОДЕ РАБОТАЕТ. НО КАК-ТО КОРЯВЕНЬКО, ПО МОЕМУ.
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LambdaFunctionInter {
     int x = 9;
     int y = 5;
@@ -12,22 +16,33 @@ public class LambdaFunctionInter {
 
     public static void main(String[] args) {
 
+        TestLamb tl=new TestLamb();
+        int[]  arr={1,2,3,4,5,6,7,8,9,11};
+        System.out.println(Arrays.toString(tl.filtr(arr,(w)-> w % 2 == 0)));
 
 
-        ClassLamThree ololo = new ClassLamThree();
-        ololo.arrOlolo(new int[]{5, 8, 9, 10});
-
-        ClassLamTwo x = new ClassLamTwo();
-        System.out.println(x.trimX("    5 4 4   ", 5));
-
-        LambdaFunctionInter lamb1 = new LambdaFunctionInter();
-        lamb1.sum(new LambdaOne() {
-
-            @Override
-            public int sum(int x, int y) {
-                return x + y;
-            }
-        });
+//        LambdaOne one = (x, y) -> {
+//            System.out.println("sdds");
+//            return x + y;
+//        };
+//
+//        LambdaTwo two = (x, y) -> {
+//            return x.trim();
+//        };
+//
+//        ClassLamThree ololo = new ClassLamThree();
+//        ololo.arrOlolo(new int[]{5, 8, 9, 10});
+//
+//        ClassLamTwo x = new ClassLamTwo();
+//        System.out.println(x.trimX("    5 4 4   ", 5));
+//
+//        LambdaFunctionInter lamb1 = new LambdaFunctionInter();
+//        lamb1.sum(new LambdaOne() {
+//            @Override
+//            public int sum(int x, int y) {
+//                return x + y;
+//            }
+//        });
     }
 
 }
@@ -54,8 +69,8 @@ interface LambdaMoreFive {
 
     default int[] isMore(int[] ololo) {
         int[] ololoMore = new int[ololo.length];
-        for (int i = 0 ; i > ololo.length; i++) {
-            if(ololo[i] > 5){
+        for (int i = 0; i > ololo.length; i++) {
+            if (ololo[i] > 5) {
                 ololoMore[i] = ololo[i];
             }
         }
@@ -63,17 +78,13 @@ interface LambdaMoreFive {
     }
 }
 
-class MoreFive implements LambdaMoreFive{
+class MoreFive implements LambdaMoreFive {
 
     @Override
     public int[] moreFive(int[] ololo) {
         return new int[0];
     }
 
-    @Override
-    public int[] isMore(int[] ololo) {
-        return new int[0];
-    }
 }
 
 class ClassLamTwo implements LambdaTwo {
@@ -91,5 +102,26 @@ class ClassLamThree implements LambdaThree {
         for (int temp : ololo) {
             System.out.println(temp);
         }
+    }
+}
+
+interface Condition {
+    boolean equ(int element);
+}
+
+class TestLamb {
+
+
+    Integer[] filtr(int[] arr, Condition condition) {
+
+        List<Integer> arrList = new ArrayList<>();
+        for (int temp : arr) {
+            if (condition.equ(temp)) {
+                arrList.add(temp);
+            }
+        }
+
+        Integer[] result = new Integer[arrList.size()];
+        return arrList.toArray(result);
     }
 }
