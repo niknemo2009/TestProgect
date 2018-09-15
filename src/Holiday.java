8ff6d51b0d6984d9db6eb18107f80b2bb9940e9a
serg722 <722kurmach@gmail.com>
2018-09-15 13:12:04 +0300

    FunctionalInterface 13.09

--- src/Holiday.java
+++ src/Holiday.java
@@ -8,23 +8,27 @@ import java.util.TreeSet;
 public class Holiday {
     private static Set<LocalDate> holidays = new TreeSet();
 
-//    static {
-//
-//    }
+    static {
+        int currentYear = LocalDate.now().getYear();
+        LocalDate temp = LocalDate.of(currentYear, 1, 1);
+        while (temp.getYear() == currentYear) {
+            if (temp.getDayOfWeek().getValue() == 7 || temp.getDayOfWeek().getValue() == 6) {
+                holidays.add(temp);
+            }
+            temp = temp.plusDays(1);
+        }
+    }
 
     public void addHoliday(LocalDate date) {
         holidays.add(date);
     }
 
-    public void delHoliday(LocalDate date) {
-        holidays.remove(date);
-    }
 
     public boolean isHoliday(LocalDate date) {
-       boolean result = false;
+        boolean result = false;
         for (LocalDate temp : holidays) {
             if (date.equals(temp)) {
-               result = true;
+                result = true;
             }
             break;
         }
@@ -36,13 +40,22 @@ public class Holiday {
     }
 
     public void holidayPeriod(LocalDate start, LocalDate stop) {
+        LocalDate temp = start.isBefore(stop)?start:stop;
+        LocalDate cond=start.isBefore(stop)?stop:start;
 
-    }
+            while (temp.isBefore(cond)) {
+                if (holidays.contains(temp)) {
+                    System.out.println(temp);
+                }
+                temp = temp.plusDays(1);
+            }
+        }
+
 
     public void replaseHoliday(LocalDate date) {
         for (LocalDate temp : holidays) {
             if (date.equals(temp)) {
-               holidays.remove(date);
+                holidays.remove(date);
             }
             break;
         }
@@ -57,8 +70,9 @@ public class Holiday {
         hol1.addHoliday(LocalDate.of(2018, 9, 1));
 //        hol1.delHoliday(LocalDate.of(2018, 8, 24));
         System.out.println(hol1.isHoliday(LocalDate.of(2018, 8, 25)));
-
+hol1.holidayPeriod(LocalDate.of(2018, 9, 30), LocalDate.of(2018,8, 31));
         System.out.println(holidays);
+        System.out.println(LocalDate.now().getDayOfWeek().getValue());
     }
 }
 
